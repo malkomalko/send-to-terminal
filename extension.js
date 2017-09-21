3e3e26acff3c48c9950ab6ab0a2a321b5c218dbb
Robert Malko <rmalko@doximity.com>
2017-09-21 09:57:09 -0700

    start to stub out flow of running commands based on file patterns

--- extension.js
+++ extension.js
@@ -10,8 +10,7 @@ function activate(context) {
     if (!editor) {
       return
     }
-    var command = `echo '${editor.document.fileName}'`
-    runCommand(command, editor, config)
+    runCommand(editor, config)
   })
   context.subscriptions.push(disposable)
 }
@@ -20,13 +19,44 @@ exports.activate = activate
 function deactivate() {}
 exports.deactivate = deactivate
 
+function buildCommand(command) {
+  return command.cmd
+}
+
 function clearBeforeRun(config) {
   return config.get('clearBeforeRun')
 }
 
-function runCommand(command, editor, config) {
+function commands(config) {
+  return config.get('commands') || []
+}
+
+function matchPattern(pattern, fileName) {
+  return pattern && pattern.length > 0 && new RegExp(pattern).test(fileName)
+}
+
+function runCommand(editor, config) {
+  var _commands = commands(config)
+  if (_commands.length === 0) {
+    return
+  }
   var column = editor.viewColumn
-  command = clearBeforeRun(config) ? ` clear; ${command}` : ` ${command}`
+  var document = editor.document
+  var fileName = document.fileName
+
+  var command = _commands.find((item) => {
+    var pattern = item.match || ''
+    var isMatch = pattern.length === 0 || matchPattern(pattern, fileName)
+    return isMatch
+  })
+
+  if (command == null) {
+    return
+  }
+
+  var _command = buildCommand(command)
+
+  command = clearBeforeRun(config) ? ` clear; ${_command}` : ` ${_command}`
   ncp.paste((err, clipboard) => {
     ncp.copy(command + EOL, () => {
       vscode.commands.executeCommand('workbench.action.terminal.focus').then(() => {
